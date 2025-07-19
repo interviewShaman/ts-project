e523c285ef7543b9a83e0fd279c138350b4169ca
Zaman <Zamanmohamed97@gmail.com>
2025-07-19 14:35:36 +0530

    Revert "add"
    This reverts commit 8846fd3613586561d842a192c3053c34bb03733b.

--- src/10-indexOf/indexOf.ts
+++
@@ -1,4 +0,0 @@
-const array = [10, 20, 30, 40, 50];
-const index = array.indexOf(30);
-
-console.log(index); // Output: 2
--- src/16-promise/01-promise-resolve-and-reject/01-Promise-resolve-and-reject.ts
+++
@@ -1,4 +0,0 @@
-// ✅ Creates a promise that resolves with a given value
-const ready = Promise.resolve("I'm done!");
-// ✅ Creates a promise that rejects with a given reason
-const error = Promise.reject("Something went wrong");
